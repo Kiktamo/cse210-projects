d1f2ff850566f1b6d8edf86925c22aff74158afc
Kiktamo <mccrary@gmx.us>
2024-02-29 16:38:54 -0500

    Prep2 Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else 
+        {
+            sign = "-";
+        }
+
+        if (grade <= 96 && grade >= 60) 
+        {
+            Console.WriteLine($"Your grade is {sign}{letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations on Passing!");
+        }
+        else
+        {
+            Console.WriteLine($"Try again next time.");
+        }
     }
 }
