45db251fa16767900d406fc28f47da09d619ed38
Kiktamo <mccrary@gmx.us>
2024-03-18 19:40:54 -0400

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Alex Mercer", "Geography");
+        Console.WriteLine("Assignment Test");
+        Console.WriteLine(a1.GetSummary() + "\n");
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Samuel Jacobs", "Fractions", "7.3", "8-19");
+        Console.WriteLine("MathAssignment Test");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList() + "\n");
+
+        WritingAssignment a3 = new WritingAssignment("Jenna Grace", "European History", "Rise of the Industrial Revolution");
+        Console.WriteLine("WritingAssignment Test");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation() + "\n");
     }
 }
