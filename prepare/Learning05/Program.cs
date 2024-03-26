0076a0df899815ea228f40dcf6ce57a03731cefe
Kiktamo <mccrary@gmx.us>
2024-03-25 21:33:04 -0400

    Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Purple", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Orange", 10, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 8);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"This {shape.GetColor()} shape, has an area of {shape.GetArea()}");          
+        }
     }
 }
